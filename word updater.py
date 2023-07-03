cab4d58b56e4249a039f674dc201cd1a85a18266
DESKTOP-LA1PA5D\andi <bantutner@gmail.com>
2023-07-03 17:21:26 +0800

    no message

--- word updater.py
+++ word updater.py
@@ -5,13 +5,15 @@ def update_word_template(template_filename, output_filename):
     document = Document(template_filename)
 
     # Get user inputs
-    name = input("Enter name: ")
-    date = input("Enter date: ")
-    serial_number = input("Enter serial number: ")
-    company_name = input ("Enter company name: ")
+    name = input('Enter name: ')
+    date = input('Enter date(DD/MM/YY): ')
+    serial_number = input('Enter serial number: ')
+    company_name = input ('Enter company name: ')
     id = input ('Enter customer ID: ')
-    Tin = input ("Enter time in : ")
-    Tout = input ('Enter time out : ')
+    Tin = input ('Enter time in (24Hr): ')
+    Tout = input ('Enter time out (24Hr): ')
+    Temi = input ('Enter temi version: ' )
+    DOW = input ('Enter Description of work: ' )
 
 
     # Update placeholders in the document with user inputs
@@ -26,12 +28,30 @@ def update_word_template(template_filename, output_filename):
             paragraph.text = paragraph.text.replace("<serial>", serial_number)
         if "<company>" in paragraph.text:
             paragraph.text = paragraph.text.replace("<company>", company_name)
-        if "<ID>" in paragraph.text:
+        if "<id>" in paragraph.text:
             paragraph.text = paragraph.text.replace("<id>", id)   
         if "<tin>" in paragraph.text:
             paragraph.text = paragraph.text.replace("<tin>", Tin)
         if "<tout>" in paragraph.text:
             paragraph.text = paragraph.text.replace("<tout>", Tout)
+        if "<Temi>" in paragraph.text:
+            paragraph.text = paragraph.text.replace("<Temi>", Temi)
+
+        # Update placeholders in tables
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    for run in paragraph.runs:
+                        if "<id>" in run.text:
+                            run.text = run.text.replace("<id>", id)
+                            print("run id")
+                        if "<date>" in run.text:
+                            run.text = run.text.replace("<date>", date)
+                            print("run date")
+                        if "<DOW>" in run.text:
+                            run.text = run.text.replace("<DOW>", DOW)
+                            print("dow")
 
     # Save the updated document as a new Word file
     document.save(output_filename)
