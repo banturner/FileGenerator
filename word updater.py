c43d0289eed521bfcc7967b50655551b6e5c7317
DESKTOP-LA1PA5D\andi <bantutner@gmail.com>
2023-07-25 22:04:06 +0800

    no message

--- word updater.py
+++ word updater.py
@@ -1,64 +1,62 @@
-from docx import Document
+import docx
 
-def update_word_template(template_filename, output_filename):
-    # Open the template document
-    document = Document(template_filename)
-
-    # Get user inputs
-    name = input('Enter name: ')
-    date = input('Enter date(DD/MM/YY): ')
-    serial_number = input('Enter serial number: ')
-    company_name = input ('Enter company name: ')
-    id = input ('Enter customer ID: ')
-    Tin = input ('Enter time in (24Hr): ')
-    Tout = input ('Enter time out (24Hr): ')
-    Temi = input ('Enter temi version: ' )
-    DOW = input ('Enter Description of work: ' )
-
-
-    # Update placeholders in the document with user inputs
+def update_placeholders(document, placeholders):
     for paragraph in document.paragraphs:
-        print(paragraph.text)
-        
-        if "<name>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<name>", name)
-        if "<date>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<date>", date)
-        if "<serial>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<serial>", serial_number)
-        if "<company>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<company>", company_name)
-        if "<id>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<id>", id)   
-        if "<tin>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<tin>", Tin)
-        if "<tout>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<tout>", Tout)
-        if "<Temi>" in paragraph.text:
-            paragraph.text = paragraph.text.replace("<Temi>", Temi)
+        for run in paragraph.runs:
+            for placeholder, value in placeholders.items():
+                if placeholder in run.text:
+                    run.text = run.text.replace(placeholder, value)
 
-        # Update placeholders in tables
     for table in document.tables:
         for row in table.rows:
             for cell in row.cells:
                 for paragraph in cell.paragraphs:
                     for run in paragraph.runs:
-                        if "<id>" in run.text:
-                            run.text = run.text.replace("<id>", id)
-                            print("run id")
-                        if "<date>" in run.text:
-                            run.text = run.text.replace("<date>", date)
-                            print("run date")
-                        if "<DOW>" in run.text:
-                            run.text = run.text.replace("<DOW>", DOW)
-                            print("dow")
+                        for placeholder, value in placeholders.items():
+                            if placeholder in run.text:
+                                run.text = run.text.replace(placeholder, value)
 
-    # Save the updated document as a new Word file
+def main():
+    template_filename = "template2.docx"
+    output_filename = "output.docx"
+
+    name = input("Enter name: ")
+    date = input("Enter date(DD/MM/YY): ")
+    serial_number = input("Enter serial number: ")
+    company_name = input("Enter company name: ")
+    id = input("Enter customer ID: ")
+    Tin = input("Enter time in (24Hr): ")
+    Tout = input("Enter time out (24Hr): ")
+    Temi = input("Enter temi version: ")
+    DOW = input("Enter description of work: ")
+    pn = input("Select the part number: ")
+    pd = input("Select the part description: ")
+    qty = input("Enter the quantity: ")
+    price = input("Select NA or As Quoted: ")
+    comments = input("Enter the comments: ")
+
+
+    placeholders = {
+        "name": name,
+        "date": date,
+        "serial": serial_number,
+        "company": company_name,
+        "id": id,
+        "tin": Tin,
+        "tout": Tout,
+        "Temi": Temi,
+        "DOW": DOW,
+        "pn":pn,
+        "pd":pd,
+        "qty":qty,
+        "price":price,
+        "comments":comments,
+
+    }
+
+    document = docx.Document(template_filename)
+    update_placeholders(document, placeholders)
     document.save(output_filename)
-    print(f"Updated Word file '{output_filename}' generated successfully.")
 
-# Example usage
-template_filename = "template.docx"
-output_filename = "output.docx"
-
-update_word_template(template_filename, output_filename)
+if __name__ == "__main__":
+    main()
